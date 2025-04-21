0bee90987eb18644e8ec80233d67d9d5517f538c
sara00abbas <sara.abbas.ab@gmail.com>
2025-04-21 19:38:51 +0300

    edit

--- lib/data/models/company_ads.dart
+++ lib/data/models/company_ads.dart
@@ -1,13 +1,13 @@
 class Company {
   final String name;
-  final String? logoUrl;  // إضافة خاصية الشعار
+  final String? logoUrl;
 
   Company({required this.name, this.logoUrl});
 
   factory Company.fromJson(Map<String, dynamic> json) {
     return Company(
       name: json['company'],
-      logoUrl: json['company_logo'], // تأكد من وجود هذه الخاصية في الـ JSON
+      logoUrl: json['company_logo'], 
     );
   }
 }

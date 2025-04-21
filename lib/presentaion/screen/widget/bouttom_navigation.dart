b7f61b40772a26eb701eab313e53c4d73fa02a1e
sara00abbas <sara.abbas.ab@gmail.com>
2025-04-21 18:24:10 +0300

    fix

--- lib/presentaion/screen/widget/bouttom_navigation.dart
+++ lib/presentaion/screen/widget/bouttom_navigation.dart
@@ -17,12 +17,8 @@ class _BottomNavigationState extends State<BottomNavigation> {
   int myCurrentIndex = 0;
   final List<Widget> pages = const [
     HomePage(),
-<<<<<<< HEAD
-    Welcomebot(),
     Searchjobscreen(),
-=======
     Allopportunity(),
->>>>>>> 38f9cabbcc49e8fe98756662e14399204eb77807
     ResumeEvaluatorApp(),
     Welcomebot(),
   ];
